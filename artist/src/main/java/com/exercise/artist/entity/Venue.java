e209c01ca5e07aff319859eba9777df7b0248341
jared <jared@DESKTOP-N8GAIB4>
2022-07-21 00:10:12 -0400

    Removed unused library

--- artist/src/main/java/com/exercise/artist/entity/Venue.java
+++ artist/src/main/java/com/exercise/artist/entity/Venue.java
@@ -2,15 +2,7 @@ package com.exercise.artist.entity;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor 
 public class Venue implements Serializable{
 
 	/**
