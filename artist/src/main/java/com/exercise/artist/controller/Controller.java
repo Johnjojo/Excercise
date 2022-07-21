6a5b3155dbf180f254660ba091c3a3cd2b6c2ad0
jared <jared@DESKTOP-N8GAIB4>
2022-07-20 23:22:39 -0400

    Code clean

--- artist/src/main/java/com/exercise/artist/controller/Controller.java
+++ artist/src/main/java/com/exercise/artist/controller/Controller.java
@@ -1,23 +1,16 @@
 package com.exercise.artist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.exercise.artist.entity.Artist;
 import com.exercise.artist.service.RestServiceImpl;
 
 @RestController
-public class controller {
+public class Controller {
 	
 	@Autowired
 	RestServiceImpl restService;
